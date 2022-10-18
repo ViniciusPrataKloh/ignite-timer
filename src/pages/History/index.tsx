90fb557c73dc096d4ee7ac47628792fdf8e3623d
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2022-10-17 22:12:07 -0300

    History Page and Styled

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,46 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
-    return <h1>History</h1>
+    return (
+        <HistoryContainer>
+            <h1>Meu histórico</h1>
+
+            <HistoryList>
+                <table>
+                    <thead>
+                        <th>Tarefa</th>
+                        <th>Duração</th>
+                        <th>Início</th>
+                        <th>Status</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Criar o projeto Ignite Timer</td>
+                            <td>6 horas</td>
+                            <td>Há cerca de 1 hora</td>
+                            <td>Andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Criar o projeto Ignite Timer</td>
+                            <td>6 horas</td>
+                            <td>Há cerca de 1 hora</td>
+                            <td>Andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Criar o projeto Ignite Timer</td>
+                            <td>6 horas</td>
+                            <td>Há cerca de 1 hora</td>
+                            <td>Andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Criar o projeto Ignite Timer</td>
+                            <td>6 horas</td>
+                            <td>Há cerca de 1 hora</td>
+                            <td>Andamento</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+        </HistoryContainer>
+    )
 }
