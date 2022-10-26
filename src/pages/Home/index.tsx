9c01d95a9cef08125f83a104261c74fad3b0096c
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2022-10-26 18:36:24 -0300

    Create multi cycles | Set document title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -58,6 +58,7 @@ export function Home() {
 
         setCycles((state) => [...state, newCycle])
         setActiveCycleId(id)
+        setAmountSecondsPassed(0)
 
         reset()
     }
@@ -65,12 +66,18 @@ export function Home() {
     const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
     useEffect(() => {
+        let interval: ReturnType<typeof setInterval> | undefined
+
         if (activeCycle) {
-            setInterval(() => {
+            interval = setInterval(() => {
                 setAmountSecondsPassed(
                     differenceInSeconds(new Date(), activeCycle.startDate),
                 )
             }, 1000)
+        }
+
+        return () => {
+            clearInterval(interval)
         }
     }, [activeCycle])
 
@@ -79,6 +86,12 @@ export function Home() {
 
     const minutes = String(Math.floor(currentSeconds / 60)).padStart(2, '0')
     const seconds = String(currentSeconds % 60).padStart(2, '0')
+
+    useEffect(() => {
+        if (activeCycle) {
+            document.title = `${activeCycle.task} - ${minutes}:${seconds}`
+        }
+    }, [minutes, seconds, activeCycle])
 
     const task = watch('task')
     const minutesAmount = watch('minutesAmount')
