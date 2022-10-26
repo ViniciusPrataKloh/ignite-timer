aab0cf85723f947ed4b8c4aceda064136450c739
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2022-10-26 19:08:11 -0300

    Interrupt cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Play } from 'phosphor-react'
+import { HandPalm, Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
@@ -11,6 +11,7 @@ import {
     MinutesAmountInput,
     Separator,
     StartCountdownButton,
+    StopCountdownButton,
     // eslint-disable-next-line prettier/prettier
     TaskInput
 } from './styles'
@@ -31,6 +32,7 @@ interface Cycle {
     task: string
     minutesAmount: number
     startDate: Date
+    interruptedDate?: Date
 }
 
 export function Home() {
@@ -46,7 +48,7 @@ export function Home() {
         },
     })
 
-    function handleCreateNewCycle(data: NewCycleFormData): void {
+    function handleStartNewCycle(data: NewCycleFormData): void {
         const id = uuid()
 
         const newCycle: Cycle = {
@@ -61,6 +63,19 @@ export function Home() {
         setAmountSecondsPassed(0)
 
         reset()
+    }
+
+    function handleStopCycle() {
+        setCycles(
+            cycles.map((cycle) => {
+                if (cycle.id === activeCycleId) {
+                    return { ...cycle, interruptedDate: new Date() }
+                } else {
+                    return cycle
+                }
+            }),
+        )
+        setActiveCycleId(null)
     }
 
     const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
@@ -91,24 +106,31 @@ export function Home() {
         if (activeCycle) {
             document.title = `${activeCycle.task} - ${minutes}:${seconds}`
         }
+
+        return () => {
+            document.title = 'Ignite Timer'
+        }
     }, [minutes, seconds, activeCycle])
 
     const task = watch('task')
     const minutesAmount = watch('minutesAmount')
     const isInputEmpty: boolean = !task || !minutesAmount
 
+    console.log(cycles)
+
     return (
         <HomeContainer>
             <form
                 action=""
                 id="timerForm"
-                onSubmit={handleSubmit(handleCreateNewCycle)}
+                onSubmit={handleSubmit(handleStartNewCycle)}
             >
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput
                         placeholder="Dê um nome para o seu projeto"
                         list="task-suggestions"
+                        disabled={!!activeCycle}
                         {...register('task')}
                     />
 
@@ -126,6 +148,7 @@ export function Home() {
                         step={5}
                         min={5}
                         max={60}
+                        disabled={!!activeCycle}
                         {...register('minutesAmount', { valueAsNumber: true })}
                     />
 
@@ -140,14 +163,24 @@ export function Home() {
                     <span>{seconds[1]}</span>
                 </CountdownContainer>
 
-                <StartCountdownButton
-                    type="submit"
-                    form="timerForm"
-                    disabled={isInputEmpty}
-                >
-                    <Play size={24} />
-                    Começar
-                </StartCountdownButton>
+                {activeCycle ? (
+                    <StopCountdownButton
+                        type="button"
+                        onClick={handleStopCycle}
+                    >
+                        <HandPalm size={24} />
+                        Interromper
+                    </StopCountdownButton>
+                ) : (
+                    <StartCountdownButton
+                        type="submit"
+                        form="timerForm"
+                        disabled={isInputEmpty}
+                    >
+                        <Play size={24} />
+                        Começar
+                    </StartCountdownButton>
+                )}
             </form>
         </HomeContainer>
     )
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -85,7 +85,8 @@ export const Separator = styled.div`
 
     color: ${(props) => props.theme['green-500']};
 `
-export const StartCountdownButton = styled.button`
+
+export const BaseCountdownButton = styled.button`
     display: flex;
     align-items: center;
     justify-content: center;
@@ -99,15 +100,26 @@ export const StartCountdownButton = styled.button`
     font-weight: bold;
     cursor: pointer;
 
-    background: ${(props) => props.theme['green-500']};
     color: ${(props) => props.theme['gray-100']};
-
-    &:not(:disabled):hover {
-        background: ${(props) => props.theme['green-700']};
-    }
 
     &:disabled {
         opacity: 0.7;
         cursor: not-allowed;
+    }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+    background: ${(props) => props.theme['green-500']};
+
+    &:not(:disabled):hover {
+        background: ${(props) => props.theme['green-700']};
+    }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+    background: ${(props) => props.theme['red-500']};
+
+    &:not(:disabled):hover {
+        background: ${(props) => props.theme['red-700']};
     }
 `
